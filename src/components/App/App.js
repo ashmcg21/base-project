ab4043fea6470f170a0b33626d0a2fb51b04bf08
Ashleigh McGinnis <ashleighmcginnis@MacBook-Pro.lan>
2020-07-26 13:12:57 -0500

    initial commit - base project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
-import FeelingsPage from '../../Pages/FeelingsPage/FeelingsPage';
+import FeelingsPage from '../Pages/FeelingsPage';
+
 
 class App extends Component {
   render() {
